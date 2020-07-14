8ffd7b670a7f591602129ffbc016013dcd35a2e6
Rajeve Subnaik <rajevesubnaik@gmail.com>
2020-07-14 19:08:25 -0400

    make the image buttons slide the image

--- js/app.js
+++ js/app.js
@@ -1,35 +1,25 @@
-// immediate invoked function expression
+const imgContainer = document.querySelector('.img-container');
+const btnLeft = document.querySelector('.btn-left');
+const btnRight = document.querySelector('.btn-right');
 
-(function() {
-  const pictures = [
-    "contBcg-0",
-    "contBcg-1",
-    "contBcg-2",
-    "contBcg-3",
-    "contBcg-4"
-  ];
+const imgArray = ['contBcg-0', 'contBcg-1', 'contBcg-2', 'contBcg-3', 'contBcg-4'];
+let counter = 0;
 
-  //select both left and right button. Add event listeners
-  const buttons = document.querySelectorAll('.btn')
-  const imageDIV = document.querySelector('.img-container')
-  let counter = 0
+function imgSlide(imgCount) {
+  if (imgCount <= -1) {
+    counter = imgArray.length - 1;
+  } else if (imgCount >= imgArray.length) {
+    counter = 0;
+  }
+  imgContainer.style.backgroundImage = `url('img/${imgArray[counter]}.jpeg')`;
+}
 
-  buttons.forEach(function(button){
-    button.addEventListener('click', function(e){
-      if (button.classList.contains('btn-left')){
-        counter--
-        if(counter < 0){
-          counter = pictures.length -1
-        }
-        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
-      }
-      if (button.classList.contains('btn-right')){
-        counter++
-        if(counter > pictures.length -1){
-          counter = 0
-        }
-        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
-      }
-    })
-  })
-})();
+btnLeft.addEventListener('click', () => {
+  counter --;
+  imgSlide(counter);
+})
+
+btnRight.addEventListener('click', () => {
+  counter ++;
+  imgSlide(counter);
+})
